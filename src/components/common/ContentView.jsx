4bebe76cf13a92ceb0378a1146d73e5e2183ac37
Sujith P <sujithp2690@gmail.com>
2024-11-01 14:05:50 +0530

    about page completed

--- src/components/common/ContentView.jsx
+++ src/components/common/ContentView.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
-import image1 from '../../assets/images/aboutPage/vision.png'
 
-const Vision = () => {
-    const heading = 'Transforming Visions into Technological Reality'
-    const content = 'Where AI Meets Integrity At AUTH AI SOLUTIONS, we leverage the transformative power of artificial intelligence to catalyse remarkable change across industries. Our unwaveringcommitment to transparency and reliability is what truly sets us apart in a crowded marketplace. '
-    const content2 = 'Our innovative solutions are designed not just to meet the demands of today but to anticipate the challenges of tomorrow, ensuring that your business stays ahead of the curve. What drives us is our steadfast adherence to ethical principles and integrity. We believe that true innovation is grounded in responsibility, fostering trust and long-term partnerships with our clients. Together, let’s unlock the potential of AI and transform your vision into reality!'
+const ContentView = ({ content, image1, content2,heading }) => {
     return (
         <div className="flex justify-center items-center min-h-[150px] bg-[#060F1A]">
             <div className="flex flex-col md:flex-row items-center justify-center gap-4 max-w-5xl p-4 relative">
@@ -18,7 +14,7 @@ const Vision = () => {
                             />
                         </div>
                         <div className=" flex w-full flex-col md:gap-6 items-start justify-start bg-[#060F1AE5] md:p-4 md:static md:bg-transparent text-left">
-                            <p className="text-[#22AAD2] text-[12px] md:text-lg">― Mobile Applications</p>
+                            {/* <p className="text-[#22AAD2] text-[12px] md:text-lg">― Mobile Applications</p> */}
                             <h3 className="text-lg md:text-3xl font-bold text-white">{heading}</h3>
                             <p className="text-[#E3E3E3] text-[12px] md:text-sm font-thin ">
                                 {content}
@@ -36,4 +32,4 @@ const Vision = () => {
     )
 }
 
-export default Vision
+export default ContentView
--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -9,17 +9,26 @@ import Team from '../aboutPageCom/Team'
 import Testimonials from '../common/Testimonials'
 import Happy from '../common/Happy'
 import Footer from '../common/Footer'
+import ContentView from '../common/ContentView'
+import GoalImage from '../../assets/images/aboutPage/goals.png'
+import missionImage1 from '../../assets/images/aboutPage/mission.png'
+
 
 const AboutPage = () => {
     const heading = 'About Us'
     const content = 'At AUTH AI SOLUTIONS, we leverage the transformative power of artificial intelligence to catalyze remarkable change across industries. Our unwavering commitment to transparency and reliability is what truly sets us apart in a crowded market place .We empower organizations with cutting-edge AI technologies that elevate data-driven insights, streamline complex processes, and ignite strategic growth.'
+    const content1 = "At AUTH AI SOLUTIONS, we don’t just build AI; we craft intelligent ecosystems that evolve and thrive alongside your business.Situated in the heart of Bengaluru's vibrant innovation hub, our dedicated team of tech-savvy engineers, data scientists, and AI visionaries is driven by a singular mission: to redefine what’s possible with artificial intelligence.Our diverse expertise spans multiple industries, empowering businesses of all sizes to harness the transformative power of AI and unlock invaluable data-driven insights.we expertly blend cutting-edge technology with a deep understanding of human potential. Whether we’re teaching machines to understand and i"
+    const content2 = "inter prehumen language, crafting in tell igent automation systems, or transforming intricate data into actionable insights, we continually push the boundaries of what’s possible. In an era where AI is rapidly advancing, we recognize that trust is the cornerstone of meaning ful innovation. From every line of code to every strateg ic initiative, weinfuse our work with commitment to ethical AI , ensuring fairness, accountability, and long-term sustainability. What truly sets us apart is our unwavering commitment to integrity and transparency."
+    const headingGoal = 'Our Visionary Goals'
+    const missionContent = 'is to empower innovation through ethical AI, ensuring that every technological advancement is firmly rooted in integrity and responsibility. We recognize that innovation is not merely about cutting-edge technology; it must also align with ethical principles that enhance trust and accountability. As such, we are dedicated to catalyzing transformative change across various industries, leveraging the immense power of artificial intelligence to tackle unique challenges and unlock new opportunities for growth and efficiency.'
+
     return (
         <>
             <CommonHero heading={heading} content={content} />
             <Vision />
-            <Goals />
+            <ContentView heading={headingGoal} image1={GoalImage} content={content1} content2={content2} />
             <Clients />
-            <Mission />
+            <ContentView image1={missionImage1} content={missionContent} />
             <Something />
             <Team />
             <Testimonials />
