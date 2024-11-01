28a4fc24402820d692a37349a7538959b7a96501
Sujith P <sujithp2690@gmail.com>
2024-11-01 12:05:39 +0530

    service details path added

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -62,6 +62,9 @@ const Navbar = () => {
                                     <Link onClick={() => setIsDropdownOpen(false)} to="/services/web_application" className="block px-4 py-2 text-sm text-white hover:text-black hover:bg-gray-200 hover:rounded">
                                         Web Application
                                     </Link>
+                                    <Link onClick={() => setIsDropdownOpen(false)} to="/services" className="block px-4 py-2 text-sm text-white hover:text-black hover:bg-gray-200 hover:rounded">
+                                        Service Details
+                                    </Link>
                                 </div>
                             )}
                         </div>
